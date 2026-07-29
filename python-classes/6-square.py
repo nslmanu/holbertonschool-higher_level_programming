82bdb08c9622aa4a19c73d54b5639dca54437d5b
nslmanu <nslmanu@me.com>
2026-07-29 14:43:56 +0200

    first commit

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,4 +34,22 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Récupère la position du carré."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Définit la position avec validation.
+
+        Raises:
+            TypeError: si value n'est pas un tuple de 2 entiers positifs.
+        """
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
 
