65adbde7a4f5ea68429ae5771cbd241b81c1becd
nslmanu <nslmanu@me.com>
2026-07-29 15:56:08 +0200

    first commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""addition d'entiers
+
+add_integer additionne deux nombres
+après validation et format en int.
+"""
+
+
+def add_integer(a, b=98):
+    """addition de a et b, bkp en int.
+
+    result TypeError si a ou b n'est ni int ni float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
