5f94f62525d7f8bafe85cfd297ccb3ee391b8ba7
nslmanu <nslmanu@me.com>
2026-07-29 16:15:47 +0200

    first commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """addition d'entiers
 
-add_integer additionne deux nombres
+add_integer ajoute deux nombres
 après validation et format en int.
 """
 
 
 def add_integer(a, b=98):
     """addition de a et b, bkp en int.
-
     result TypeError si a ou b n'est ni int ni float.
     """
     if not isinstance(a, (int, float)):
