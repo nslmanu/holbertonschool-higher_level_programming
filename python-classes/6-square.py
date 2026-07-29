0f54714c623238e83a430b7b52bb24e10b345f49
nslmanu <nslmanu@me.com>
2026-07-29 14:48:57 +0200

    first commit

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,4 +55,13 @@ class Square:
     def area(self):
         """Retourne l'aire du carré."""
         return self.__size ** 2
-
+    
+    def my_print(self):
+        """Affiche le carré avec des #, décalé selon position."""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
