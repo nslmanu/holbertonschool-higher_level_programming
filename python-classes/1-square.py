0c55b2f7f1ee3fbc929cb988c3b952124a983d32
nslmanu <nslmanu@me.com>
2026-07-29 11:30:54 +0200

    first commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module classe Square vide."""
+
+
+class Square:
+    """Module classe Square vide."""
+
+
+    def __init__(self, size):
+        """Size prive"""
+        self.__size = size
+
