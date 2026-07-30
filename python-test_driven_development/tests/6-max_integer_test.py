65299ab6b8ac7f1cee0cac4c5d784d18cab7f688
nslmanu <nslmanu@me.com>
2026-07-30 14:56:44 +0200

    first commit

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,4 +4,11 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """Tests de la fonction max_integer."""
+
+    def test_ordered_list(self):
+        """Liste ordonnée."""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
